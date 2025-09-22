1d3be5c810c13fa62cf2cda8f136d0e46daebc10
Phuc <21521292@gm.uit.edu.vn>
2025-09-22 09:30:46 +0700

    anh nen dep

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Website đã sẵn sàng!");
+  document.body.style.backgroundImage = "url('assets/images/background.jpg')";
+  document.body.style.backgroundRepeat = "repeat";
 });
